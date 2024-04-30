6bf6188508cc2dc3fcec535b8a0f5dfbe0f205d5
FranciscoJSB12 <javif8@gmail.com>
2024-04-30 12:39:33 -0400

    feat: Add model for pokemon in database

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,24 @@
-export class Pokemon {}
+import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+    @Prop({
+        unique: true,
+        index: true
+        /*El indice sabe específicamente donde está el 
+        elemento que estás buscando*/
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true
+    })
+    nroPokemon: number;
+}
+
+/*Exportamos un schema, es el que le dice a la base 
+de datos estas son  las definiciones, reglas, columnas que quiero que uses*/
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
